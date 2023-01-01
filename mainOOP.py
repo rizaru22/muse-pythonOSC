0962d38f175946bd8be7498951da739514821f91
rizaru22 <rizaru.2.2@gmail.com>
2023-01-01 13:32:33 +0700

    each channels has label now, horee

--- mainOOP.py
+++ mainOOP.py
@@ -22,17 +22,46 @@ class Intro(tk.Tk):
         # self.resizable(False,False)
         
         #label
-        self.labelDevice=ttk.Label(self,text="Device :")
+        self.labelDevice=ttk.Label(self,text="Device ")
         self.labelDevice.grid(row=0,column=0,padx=10,pady=10,sticky="W")
             
-        #label
-        self.labelConnection=ttk.Label(self,text="")
-        self.labelConnection.grid(row=0,column=1,padx=10,pady=10,sticky="E")
+        
+        self.labelConnection=ttk.Label(self,text=":")
+        self.labelConnection.grid(row=0,column=1,padx=10,pady=10,sticky="W")
+        
+        self.labelChannel=ttk.Label(self,text="Channel")
+        self.labelChannel.grid(row=1,column=0,columnspan=2,padx=10,pady=10,sticky="NS")
+        
+        self.labelTP9=ttk.Label(self,text="TP9 ")
+        self.labelTP9.grid(row=2,column=0,padx=10,pady=10,sticky="W")
+        
+        self.labelValueTP9=ttk.Label(self,text=":")
+        self.labelValueTP9.grid(row=2,column=1,padx=10,pady=10,sticky="W")
+        
+        self.labelTP10=ttk.Label(self,text="TP10 ")
+        self.labelTP10.grid(row=3,column=0,padx=10,pady=10,sticky="W")
+        
+        self.labelValueTP10=ttk.Label(self,text=":")
+        self.labelValueTP10.grid(row=3,column=1,padx=10,pady=10,sticky="W")
+        
+        self.labelAF7=ttk.Label(self,text="AF7 ")
+        self.labelAF7.grid(row=4,column=0,padx=10,pady=10,sticky="W")
+        
+        self.labelValueAF7=ttk.Label(self,text=":")
+        self.labelValueAF7.grid(row=4,column=1,padx=10,pady=10,sticky="W")
+        
+        self.labelAF8=ttk.Label(self,text="AF8 ")
+        self.labelAF8.grid(row=5,column=0,padx=10,pady=10,sticky="W")
+        
+        self.labelValueAF8=ttk.Label(self,text=":")
+        self.labelValueAF8.grid(row=5,column=1,padx=10,pady=10,sticky="W")
+        
+        
         
         
         #button
         self.buttonStart=ttk.Button(self,text="Start",command=self.startAction)
-        self.buttonStart.grid(row=1,column=0,columnspan=2,padx=10,pady=10,sticky="NS")
+        self.buttonStart.grid(row=6,column=0,columnspan=2,padx=10,pady=10,sticky="NS")
         
     def startAction(self):
         self.buttonStart.config(state=tk.DISABLED)
@@ -64,13 +93,18 @@ class Intro(tk.Tk):
         for arg in args:
             # print (arg)
             if arg == 1:
-                self.labelConnection.config(text="Connected")
+                self.labelConnection.config(text=": Connected")
             else:
-                self.labelConnection.config(text="Disconnected")
+                self.labelConnection.config(text=": Disconnected")
             
     def eeg_handler(self, address, *args):
         tp9, af7, af8, tp10, au = args
-        print("tp9:"+str(tp9) + " "+"tp10:" + str(tp10) + " "+"af7:"+str(af7) + " "+"af8:"+str(af8))
+        self.labelValueTP9.config(text=":"+str(tp9))
+        self.labelValueTP10.config(text=":"+str(tp10))
+        self.labelValueAF7.config(text=":"+str(af7))
+        self.labelValueAF8.config(text=":"+str(af8))
+        
+        # print("tp9:"+str(tp9) + " "+"tp10:" + str(tp10) + " "+"af7:"+str(af7) + " "+"af8:"+str(af8))
             
 
 
